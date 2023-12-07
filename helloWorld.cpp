7ac71f3da38d33f1bea1c9bda6467c175370f99a
MilkTseng <111765204+MilkTseng@users.noreply.github.com>
2023-12-07 20:43:36 +0800

    hello world 10 times

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 // Todo: 寫一個印出"Hello World"的程式
 int main() {
-    for (int i = 0; i < 50; i++){
-        cout << i << endl;
-    }
+    for(int i = 0; i < 10; i++)
+        cout << "Hello World";
     return 0;
 }
